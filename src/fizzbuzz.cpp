3e587d12304aa19f5e5cd70b5b24d9b3412bf7a9
ashok baktha <ashok@server>
2014-09-20 16:54:09 +0530

    Helper converter function failing miserably.

--- src/fizzbuzz.cpp
+++ src/fizzbuzz.cpp
@@ -11,6 +11,24 @@ using namespace igloo;
 #include <iostream>
 using namespace std;
 
+string convert_number_to_str(int n)
+{
+	// cout << "received: "  << n << endl;
+	string conv = "";
+	char c_digit = '\0';
+	int digit;
+	for (int i = 10; n > 0; i = i*10) {
+		digit = n%i;
+		// cout << "digit: " << digit << endl;
+		c_digit = (char)(digit + '0');
+		// cout << "c_digit: " << c_digit << endl;
+		conv = c_digit + conv;
+		n = n/10;
+	}
+	// cout << "*** Converted to: " << conv << endl;
+	return conv;
+}
+
 string fizzBuzz (int i_array[], int size) {
 	string out = "";
 	int index = 0;
@@ -23,7 +41,7 @@ string fizzBuzz (int i_array[], int size) {
 		else if (size >= 1 && i_array[index] == 5)
 			out = out + "Buzz";
 		else
-			out = out + (char)(i_array[index] + '0');
+			out = out + convert_number_to_str(i_array[index]);
 
 		size--; index++;
 		if (size != 0)
@@ -67,6 +85,8 @@ Context(AFizzBuzzConverter)
 			Assert::That(fizzBuzz(array2, 2), Equals("1, 2"));
 			int array3[10] = {1, 2, 7, 13};
 			Assert::That(fizzBuzz(array3, 4), Equals("1, 2, 7, 13"));
+			int array4[10] = {1, 2, 7, 13, 222, 13340};
+			Assert::That(fizzBuzz(array4, 6), Equals("1, 2, 7, 13, 222, 13340"));
 		}
 
 	/*Spec(ShouldInstructThatARegularLeapYearIsALeapYear)

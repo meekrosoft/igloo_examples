b42fe63c85e02cd6f2a072f6732d04446965050d
ashok baktha <ashok@server>
2014-11-02 17:09:25 +0530

    Reordered the sequence of tests

--- src/make_account2.cpp
+++ src/make_account2.cpp
@@ -41,16 +41,17 @@ int make_amount(int amount, int denominations[], int dsize)
 
 Context(AAmountConverter)
 {
-	Spec(ShouldOutput0ForNegative1)
-	{
-	  int d[1] = {1};
-	  Assert::That(make_amount(-1, d, 1), Equals(0));
-	}
 	Spec(ShouldOutput1For0)
 	{
 	  int d[1] = {1};
 	  Assert::That(make_amount(0, d, 1), Equals(1));
 	}
+	Spec(ShouldOutput0ForNegative1)
+	{
+	  int d[1] = {1};
+	  Assert::That(make_amount(-1, d, 1), Equals(0));
+	}
+
 	Spec(ShouldOutput0For1AndNull)
 	{
 	  Assert::That(make_amount(1, {}, 0), Equals(0));

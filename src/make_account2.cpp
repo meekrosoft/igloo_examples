9d776532f60bd653d0b85ba075cea86d20cee5c2
ashok baktha <ashok@server>
2014-11-02 17:17:40 +0530

    Refactored and compiling and passing using vectors, done

--- src/make_account2.cpp
+++ src/make_account2.cpp
@@ -13,36 +13,33 @@ using namespace std;
 
 #include <vector>
 
-int make_amount(int amount, int denominations[], int dsize)
+int make_amount(int amount, vector<int>& denominations)
 {
 	if (amount == 0)
   	  return 1;
     if (amount < 0)
 	  return 0;
-	if (dsize == 0)
+	if (denominations.size() == 0)
 		return 0;
 
 	int current_coin = denominations[0];
-	int rest_of_coins[dsize-1];
-	int rsize = dsize-1;
-	for (int i = 1; i < dsize; i++)
-		rest_of_coins[i-1] = denominations[i];
+	vector<int> rest_of_coins (denominations.begin()+1, denominations.end());
 
 	int count = 0;
 	while (amount >= 0)
 	{
-		count = make_amount(amount, rest_of_coins, rsize);
+		count = make_amount(amount, rest_of_coins);
 		amount = amount - current_coin;
 	}
 
 	return count;
 }
 
-int make_amount_new(int amount, int denominations[], int size)
+int make_amount(int amount, int denominations[], int size)
 {
 	vector<int> v_d(denominations, denominations+size);
-	// return make_amount(amount, v_d);
-	return 0;
+	return make_amount(amount, v_d);
+
 }
 
 Context(AAmountConverter)
